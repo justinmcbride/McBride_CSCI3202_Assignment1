fa2a903d16d43a878eefeead302acd36285980f7
Justin McBride <justin@dare599z.com>
2015-08-26 22:24:42 -0600

    switched to unittesting; refined Queue

--- McBride_Assignment1.py
+++ McBride_Assignment1.py
@@ -1,47 +1,55 @@
 from __future__ import print_function
 import Queue as OriginalQueue
-
-class NotInteger(Exception):
-  def __str__(self):
-    return "Value is not integer"
+import unittest
 
 class Queue:
-  _queue = OriginalQueue.Queue()
+
+  def __init__(self):
+    self.queue = OriginalQueue.Queue()
+    self.size = 0
 
   def Put(self, item):
     if type(item) is not int:
-      raise NotInteger
-    self._queue.put(item)
-    return
+      return False
+    if self.queue.full():
+      return False
+    self.queue.put(item)
+    self.size = self.size + 1
+    return True
 
   def Get(self):
-    return self._queue.get()
+    if self.size <= 0:
+      return None
+    self.size = self.size - 1
+    return self.queue.get()
 
   def Size(self):
-    return self._queue.qsize()
+    return self.queue.qsize()
 
-def main():
-  jq = Queue()
-  print("We don't want to fail here... ", end="")
-  try:
-    jq.Put(1)
-    jq.Put(100)
-    jq.Put(1001)
-  except Exception as e:
-    print("FAIL: " + str(e))
-  else:
-    print("GOOD!")
+class test_Queue(unittest.TestCase):
+  def testPut(self):
+    q = Queue()
+    self.assertTrue(q.Put(5))
+    self.assertTrue(q.Put(-50000))
+    self.assertFalse(q.Put('fail'))
+    self.assertFalse(q.Put([0,1]))
 
-  print("We hope to fail here... ", end="")
-  try:
-    jq.Put('hello')
-  except Exception as e:
-    print("GOOD: " + str(e))
-  else:
-    print("FAIL: should have failed")
+  def testSize(self):
+    q = Queue()
+    self.assertEquals(0, q.Size())
+    [ q.Put(x) for x in xrange(0,5) ]
+    self.assertEquals(5, q.Size())
+    q.Get()
+    q.Get()
+    self.assertEquals(3, q.Size())
+
+  def testGet(self):
+    q = Queue()
+    self.assertIsNone(q.Get())
+    [ q.Put(x) for x in xrange(0,10) ]
+    [ self.assertEquals(q.Get(), x) for x in xrange(0,10) ]
 
 
-  return
 
 if __name__ == '__main__':
-  main()
+  unittest.main(verbosity=2)
